44404a33cf39a58ab228861fee0f3b89aab2efea
Georg Thiesen <georg.thiesen@rwth-aachen.de>
2024-11-14 17:18:01 +0100

    init

---
+++ server/target/debug/deps/server-097a367092701782.d
@@ -0,0 +1,9 @@
+/home/ipagaxi/Repos/WoIstDerBus/server/target/debug/deps/libserver-097a367092701782.rmeta: src/main.rs Cargo.toml
+
+/home/ipagaxi/Repos/WoIstDerBus/server/target/debug/deps/server-097a367092701782.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
