e47b2e634a3671087110cd94c19f448b4e440e29
Georg Thiesen <georg.thiesen@rwth-aachen.de>
2024-11-17 17:48:15 +0100

    added simple server listener

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,28 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let connection_ip = "127.0.0.1";
+    let connection_port = "7878";
+    let listener = TcpListener::bind(connection_ip.to_owned() + ":" + connection_port).unwrap();
+    println!("\nOpen connection on {}:{}", connection_ip, connection_port);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
 }
