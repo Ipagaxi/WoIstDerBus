a64808ba33cf23daa833186d657891d1c3c28dd4
Ipagaxi <g.thiesen@posteo.de>
2025-04-02 20:10:30 +0200

    minor changes (client)

--- client/widb-client/src/routes/BusRoutes.svelte
+++ client/widb-client/src/routes/BusRoutes.svelte
@@ -5,7 +5,11 @@
   let resp_status = 0;
 
   async function getBusRoute() {
-    const url = 'http://192.168.178.40:3000/bus_route'
+    const url = 'http://192.168.178.41:3000/bus_route'
+    const headers = {
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+      "Content-Type": "application/json"
+    }
     const request_data = `{
         "out_frwd": true,
         "arr_station": {
@@ -28,13 +32,14 @@
     try {
       const response = await fetch(url, {
         method: 'POST',
+        headers: headers,
         body: request_data
       });
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
       resp_status = response.status;
-      resp = response.statusText;
+      resp = await response.text();
       console.log(response.status); // e.g. 200
       console.log(response.statusText); // e.g. "OK"
     } catch (error) {
