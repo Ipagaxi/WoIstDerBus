b68cb6c60f99623bcac21ea48af883e717de28b0
Ipagaxi <g.thiesen@posteo.de>
2025-08-09 00:02:04 +0200

    chore (server): deleted target dir which should be ignored from now on by git

--- server/target/debug/deps/server-94b9bb8893b852bc.d
+++
@@ -1,9 +0,0 @@
-/home/ipagaxi/Repos/WoIstDerBus/server/target/debug/deps/libserver-94b9bb8893b852bc.rmeta: src/main.rs Cargo.toml
-
-/home/ipagaxi/Repos/WoIstDerBus/server/target/debug/deps/server-94b9bb8893b852bc.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
