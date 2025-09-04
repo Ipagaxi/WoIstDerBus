f8379df24f3739304c71fa51c22b6a44736a25d0
Georg Thiesen <georg.thiesen@rwth-aachen.de>
2025-09-04 16:53:09 +0200

    feat (server): parse bus name and direction text as well and send whole package to client

--- server/src/util_json.rs
+++ server/src/util_json.rs
@@ -1,6 +1,13 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::fs;
+use regex::Regex;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BusData {
+  name: String,
+  direction_text: String,
+  pos: BusPosition
+}
 
 #[derive(Debug, Serialize, Deserialize)] 
 pub struct BusPosition {
@@ -62,9 +69,92 @@ pub fn get_value_by_path<'a>(json_data: &'a Value, path: &Vec<&str>) -> Vec<&'a 
     positions
 }
 
-pub fn get_infos_of_all_busses_for_route(route_data_json: &str) -> Vec<BusPosition> {
+fn get_bus_name(info_string: &str) -> &str {
+  // hay example: "T$A=1@O=Aachen, Bushof@L=1001@a=128@$A=1@O=Aachen, Halifaxstraße@L=1427@a=128@$202509041222$202509041232$Bus   73$$1$$$$$$"
+  let re = Regex::new(r"(?<=(\$Bus\s*))\w+").unwrap();
+  if let Some(m) = re.find(info_string) {
+        &info_string[m.start()..m.end()]
+  } else { 
+    println!("No regex captures found");
+    "None"
+  }
+}
+
+fn cast_pos_value_to_struct(pos_value: Value) -> BusPosition {
+  match serde_json::from_value(pos_value) {
+    Ok(pos) => pos,
+    Err(e) => {
+      println!("Error: {}", e);
+      BusPosition {
+        x: 0,
+        y: 0
+      }
+    }
+  }
+}
+
+fn parse_bus_name(json_property_value: &Value) -> &str {
+  if let Some(info_value) = json_property_value.get("ctxRecon") {
+    match info_value.as_str() {
+      Some(info_str) => {
+        get_bus_name(info_str)
+      },
+      None => {
+        println!("Property ctxRecon found, but it is not a string?!");
+        "None"
+      }
+    }
+  } else {
+    println!("No info string with bus name found (ctxRecon)");
+    "None"
+  }
+}
+
+fn parse_bus_direction(json_property_value: &Value) -> &str {
+  if let Some(direction_value) = json_property_value.get("dirTxt") {
+    match direction_value.as_str() {
+      Some(direction_str) => direction_str,
+      None => {
+        println!("Property 'dirTxt' found, but it is not a string?!")
+        "N/A"
+      }
+    }
+  } else {
+    println!("No direction text found (dirTxt)!");
+    "N/A"
+  }
+}
+
+pub fn get_infos_of_all_busses_for_route(route_data_json: &str) -> Vec<BusData> {
   let route_data_value: Value = serde_json::from_str(route_data_json).expect("Failed to parse JSON");
 
+  let mut bus_data_vec: Vec<BusData> = Vec::new();
+  let json_jiny_l_path = vec!["svcResL", "res", "outConL", "secL", "jny", "freq", "jnyL"];
+  let value_jny_l_vec = get_value_by_path(&route_data_value, &json_jiny_l_path);
+  for jny_l_value in value_jny_l_vec {
+    if let Some(arr) = jny_l_value.as_array() {
+      for entry in arr {
+        // Only parse busses that have a live position
+        if let Some(pos_value) = entry.get("pos") {
+          let pos = cast_pos_value_to_struct(pos_value.clone());
+          
+          // parse bus name/number e.g. "33"
+          let name = parse_bus_name(&entry);
+          // parse bus direction text, e.g. "Uniklinik"
+          let direction = parse_bus_direction(&entry);
+          let bus_data = BusData {
+            name: name.to_string(),
+            direction_text: direction.to_string(),
+            pos
+          };
+          bus_data_vec.push(bus_data);
+        }
+      }
+    }
+  }
+  bus_data_vec
+
+  /*
   let json_path = vec!["svcResL", "res", "outConL", "secL", "jny", "freq", "jnyL", "pos"];
   let value_bus_positions = get_value_by_path(&route_data_value, &json_path);
   let mut bus_positions: Vec<BusPosition> = Vec::new();
@@ -79,4 +169,5 @@ pub fn get_infos_of_all_busses_for_route(route_data_json: &str) -> Vec<BusPositi
     }
   }
   bus_positions
+  */
 }
